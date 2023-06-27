113b737779f71b987ecbf2e9c589b1b54ade34e9
David Hoffmann <david.hoffmann@mail.de>
2023-06-27 07:08:19 +0000

    6 year backtests & added offset attribute

---
+++ modelling/src/forecast_util.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+
+def plot_forecast(tss:list, forecasts:list, path:str = None) -> None:
+    """
+    Plots the training target timeseries and the predicted forecast(s). 
+    If forcasts is an array of muliple forecast windows, these
+    will be ploted acordingly.
+
+    Parameters
+    ----------
+    tss 
+        Yields the corresponding ground truth series.
+    forecasts 
+        Yields the forecast timeseries.
+        
+        
+    Returns: None
+    """
+    # Plot TTS
+    for ts in tss:
+        plt.plot(ts.to_timestamp(), color="black")
+    # Plot forecast(s)
+    for forecast in forecasts:
+        forecast.plot()
+    plt.legend(["True values"], loc="upper left", fontsize="large")
+    # Save figure
+    if path:
+        plt.savefig(path)
